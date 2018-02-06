4b0e3e2f1974fe8226919a4cc3320deea92a0fa1
aneeshverma04 <aneeshvrma@yahoo.com>
2018-02-06 09:23:36 +0530

    Debugged form error

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for, session, logging, request
+from wtforms import Form, StringField , TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+# pip install Flask-WTF
+# pip install passlib
 
 app = Flask(__name__) # object called app is created of Flask class
 
@@ -22,9 +27,29 @@ def login():
 def logout():
 	return render_template('logout.html')
 
-@app.route('/signup')
+# USING WTFORMS MAKING A FORM CLASS WITH INBUILT VALIDATIONS
+class Signup(Form):
+	name = StringField('Name',[validators.Length(min = 1, max = 50)])
+	username = StringField('Username',[validators.Length(min = 4, max = 25)])
+	email = StringField('Email',[validators.Length(min = 6, max = 50)])
+	password = PasswordField('Password',[
+		validators.DataRequired(),
+		validators.EqualTo('confirm',message = 'Passwords do not match')
+		])
+	confirm = PasswordField('Confirm Password')
+
+
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-	return render_template('signup.html')
+	form = Signup(request.form)
+	if request.method == 'POST' and form.validate():
+		name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        
+	return render_template('signup.html',form = form)
 
 if __name__ == '__main__':
+	app.secret_key = 'secretZone'
 	app.run(debug = True)
