2afaf38f2087e1522e0169459b1c9245f497742f
aneeshverma04 <aneeshvrma@yahoo.com>
2018-03-30 14:32:27 +0530

    Article begin

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os,sys,math
 
-from exceptions import Exception
-from flask import Flask, render_template, flash, request, redirect, url_for, session, logging, request, send_from_directory, jsonify
-from pip._vendor.requests.compat import str
-from wtforms import Form, StringField , TextAreaField, PasswordField, validators
-from flask_mysqldb import MySQL
-from passlib.hash import sha256_crypt
 from functools import wraps
+from flask_mysqldb import MySQL
+from exceptions import Exception
 from flask_mail import Mail, Message
 from werkzeug import secure_filename
+from passlib.hash import sha256_crypt
 from werkzeug.exceptions import BadRequest
+from pip._vendor.requests.compat import str
+from wtforms import Form, StringField , TextAreaField, PasswordField, validators
+from flask import Flask, render_template, flash, request, redirect, url_for, session, logging, request, send_from_directory, jsonify
 
 from Crypto.Cipher import DES
 
@@ -143,9 +143,9 @@ def phone1():
         send_mail(suggestions)
         # get data through invisible form
         email = request.form['email']
-        category = request.form['category']
-        product = request.form['product']
         rating = request.form['starwar']
+        product = request.form['product']
+        category = request.form['category']
 
 
         #Create Cursor
@@ -1101,6 +1101,146 @@ def upload():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
+
+
+
+@app.route('/articles')
+def articles():
+    #Create cursor
+    cur = mysql.connection.cursor()
+
+    #Get articles
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template('articles.html',articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template('articles.html',msg=msg)    
+    #close connection
+    cur.close()
+
+#register form class
+@app.route('/article/<string:id>/')
+def article(id):
+    #Create cursor
+    cur = mysql.connection.cursor()
+
+    #Get articles
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+    
+    article = cur.fetchone()
+    
+    return render_template ('article.html',article=article)
+
+
+#Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    #Create cursor
+    cur = mysql.connection.cursor()
+
+    #Get articles
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template('dashboard.html',articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template('dashboard.html',msg=msg)    
+    #close connection
+    cur.close()
+
+    
+#Article Form Class
+class ArticleForm(Form):
+    title = StringField('Title',[validators.Length(min = 1 , max =200)])
+    body = TextAreaField('Body',[validators.Length(min=30)])
+    
+#Add Article
+@app.route('/add_article',methods=['GET','POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        #Exexute
+        cur.execute("INSERT INTO articles(title,body,author)VALUES(%s, %s, %s)",(title, body, session['username']))    
+
+	    #Commit to DB
+        mysql.connection.commit()
+
+        #Close connectionn
+        cur.close()
+
+        flash('Article Created', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('add_article.html',form = form)
+
+#Edit Article
+@app.route('/edit_article/<string:id>',methods=['GET','POST'])
+@is_logged_in
+def edit_article(id):
+    # Create Cursor
+    cur = mysql.connection.cursor()
+    
+    #Get article by id
+    result = cur.execute("SELECT * FROM articles WHERE id = %s",[id])
+    article = cur.fetchone()
+    
+    #Get form
+    form = ArticleForm(request.form)
+    
+    #Populate article form fields
+    form.title.data = article['title']
+    form.body.data = article['body']
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        body = request.form['body']
+
+        #Create cursor
+        cur = mysql.connection.cursor()
+
+        #Exexute
+        cur.execute("UPDATE articles SET title = %s, body = %s WHERE id = %s ",(title,body,id))
+
+        #Commit to DB
+        mysql.connection.commit()
+
+        #Close connectionn
+        cur.close()
+
+        flash('Article Updated', 'success')
+        return redirect(url_for('dashboard'))
+    return render_template('edit_article.html',form = form)        
+
+#Delete Article
+@app.route('/delete_article/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_article(id):
+    #create cursor
+    cur = mysql.connection.cursor()
+
+    #execute
+    cur.execute("DELETE FROM articles WHERE id = %s",[id])
+
+    #Commit to DB
+    mysql.connection.commit()
+
+    #Close connectionn
+    cur.close()
+
+    flash('Article Deleted', 'success')
+    return redirect(url_for('dashboard'))
+
+
+
 
 
 if __name__ == '__main__':
